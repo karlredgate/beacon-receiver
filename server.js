77fa7103074d151057ffb467a005fe139baa3969
Karl Redgate <Karl.Redgate@gmail.com>
2019-01-29 07:51:27 -0500

    initial files

---
+++ server.js
@@ -0,0 +1,79 @@
+#!/usr/bin/env node
+
+const Path = require('path');
+const Express = require('express');
+const BodyParser = require('body-parser');
+const SQLite = require('sqlite3');
+
+function bad_request( response ) {
+    response.status( 400 );
+    response.send( '{"error": "invalid"}' );
+    response.end();
+}
+
+function is_invalid( request ) {
+    // if ( typeof request.body.metrics == 'undefined' ) return true;
+    // if ( typeof request.body.dataset == 'undefined' ) return true;
+    // check "name", "frequency", "filters", "group by"
+    return false;
+}
+
+function find_config( request, response, next ) {
+    if ( typeof request.query.config_id == 'undefined' ) {
+        // Check if there are more than one for this account
+        // console.log( "missing wafconfig" );
+        return next();
+    }
+    console.log( "Look for " + request.query.config_id );
+    // lookup config object
+    next();
+}
+
+function beacon( request, response ) {
+    var data = {
+        "data": [1,2,3],
+        "request": request.body
+    };
+
+    if ( is_invalid(request) ) {
+        bad_request( response );
+        return;
+    }
+
+    // response.end( 'DATA' );
+    response.status( 200 );
+    response.set( 'Content-Type', 'application/json' );
+    response.end( JSON.stringify(data) );
+}
+
+// Change so it errors with wrong content type
+//
+function registerAPIs( app ) {
+    var parser = BodyParser.json( {type: ['application/json', 'application/*+json']} );
+    app.use( parser );
+    app.use( find_config );
+
+    app.post( '/beacon',   beacon );
+}
+
+function connected() {
+    console.log( "server started" );
+}
+
+function main( argv ) {
+    var db = new SQLite.Database('extapi.db');
+    db.close();
+
+    var port = 3200;
+    var app = Express();
+
+    app.disable( 'x-powered-by' );
+    registerAPIs( app );
+    var content = Path.join( __dirname, "/static/" );
+    app.use( Express.static(content) );
+    app.listen( port, connected );
+}
+
+main( process.argv );
+
+// vim: autoindent expandtab sw=4
